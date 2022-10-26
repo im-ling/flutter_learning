1893a7e6996d9a61cff006dfd31b8b403de4ba8a
ling <tobeabetterman_dl@163.com>
2022-10-26 16:25:17 +0800

    feat: - Create a List of Widgets - Select and item from a List - ListView.builder Widget - ListTile Widget - Next Steps

--- flutter_3.0_ tutorial/my_app/lib/main.dart
+++ flutter_3.0_ tutorial/my_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/home_page.dart';
+import 'package:my_app/profile_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -27,6 +28,10 @@ class RootPage extends StatefulWidget {
 
 class _RootPageState extends State<RootPage> {
   int currentPage = 0;
+  List<Widget> pages = const [
+    HomePage(),
+    ProfilePage(),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,7 +39,7 @@ class _RootPageState extends State<RootPage> {
         // backgroundColor: Colors.black,
         title: const Text('Flutter'),
       ),
-      body: const HomePage(),
+      body: pages[currentPage],
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           debugPrint("Floating Action Button 123");
